17f8be643d51b90ba66ea54bd8ad69b62704c8a3
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-06-13 22:13:52 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+// If you are reading this script in your web browser then
+// your server probably isn't configured properly to run
+// PHP programs!
+//
+// See the INSTALL file for more information.
+
+// Uncomment for debugging
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+
+if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
+    echo 'Couldn\'t find PHP MySQLi. It is possible that your server wasn\'t configured correctly.';
+    exit;
+}
+
+// Show a warning for users that haven't configured Bugkiller using config.ini
+if (!file_exists('config.ini')) {
+  header('HTTP/1.1 500 Internal Server Error');
+  echo '<h1>Bugkiller Error: Internal Server Error</h1>';
+  echo '<p><code>config.ini</code> does not exist. Please run <a href="/bugkiller/setup.php"><code>setup.php</code></a> to set up the bug tracker\'s configuration.</p>';
+  echo '<p>Please note that additional errors might occur when you start the bug tracker even when configured. If you do get errors like those, ensure your MySQL database exists.</p>';
+  exit;
+}
+
+// Get config values from file
+// Warning: Ensure this file is inaccessible from the web.
+$config = parse_ini_file("config.ini");
+$servername = $config['servername'];
+$username = $config['username'];
+$password = $config['password'];
+$dbname = $config['dbname'];
+$projectname = $config['projectname'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  echo "Connection failed: " . $conn->connect_error;
+  exit;
+}
+
+// Create table for bugs
+$sql = "CREATE TABLE IF NOT EXISTS bugs (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, bug_name VARCHAR(30) NOT NULL, bug_description VARCHAR(100) NOT NULL, status VARCHAR(30) NOT NULL, priority VARCHAR(30) NOT NULL, date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);";
+
+if (!$conn->query($sql) === TRUE) {
+  echo "Error creating the Bugkiller database.<br>" . $conn->error;
+  exit;
+}
+
+// Close connection
+$conn->close();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title><?php echo $projectname . " Bugkiller"; ?></title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1><?php echo $projectname . " Bugkiller"; ?></h1>
+<button class="bugkiller-button" onclick="window.location.href = '/bugkiller/reportbug.php';">Report a Bug</button>
+</body>
+</html>
