889743de1073d332ad1fd8c373c80aeed85dc00e
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-06-14 16:32:56 +0100

    Update reportbug.php

--- reportbug.php
+++ reportbug.php
@@ -1,26 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Report a Bug - <?php echo $projectname . " Bugkiller"; ?></title>
-<link rel="stylesheet" href="style.css">
-</head>
-<body>
-<h1>Report a Bug</h1>
-<?php if ($wikitextallowed == true) { echo "<p>Wikitext formatting is supported.</p>"; } ?>
-<p>Please note that <?php echo $projectname . " Bugkiller"; ?> only allows bug reports about <?php echo $projectname ?>.</p>
-<form method="post">
-        <input type="text" id="title" name="title" placeholder="Title"><br><br>
-        <textarea id="description" name="description" placeholder="Description"></textarea><br><br><label for="status"><b>Status</b><br><small>It is highly recommended to leave this as Needs Triage. B.G.M.B. is only suitable for very large bugs, like ones that occur when opening any page or signing in.</small></label><br><br><select id="status" name="status">
-        <option value="Needs Triage">Needs Triage</option>
-        <option value="B.G.M.B.">B.G.M.B. (Big Giant Monster Bug)</option>
-        <option value="High">High</option>
-        <option value="Medium">Medium</option>
-        <option value="Low">Low</option>
-        </select><br><br>
-        <input type="submit" value="Report" class="bugkiller-button">
-        </form>
-</body>
-</html>
 <?php
 $config = parse_ini_file("config.ini");
 
@@ -55,3 +32,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Report a Bug - <?php echo $projectname . " Bugkiller"; ?></title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Report a Bug</h1>
+<?php if ($wikitextallowed == true) { echo "<p>Wikitext formatting is supported.</p>"; } ?>
+<p>Please note that <?php echo $projectname . " Bugkiller"; ?> only allows bug reports about <?php echo $projectname ?>.</p>
+<form method="post">
+        <input type="text" id="title" name="title" placeholder="Title"><br><br>
+        <textarea id="description" name="description" placeholder="Description"></textarea><br><br><label for="status"><b>Status</b><br><small>It is highly recommended to leave this as Needs Triage. B.G.M.B. is only suitable for very large bugs, like ones that occur when opening any page or signing in.</small></label><br><br><select id="status" name="status">
+        <option value="Needs Triage">Needs Triage</option>
+        <option value="B.G.M.B.">B.G.M.B. (Big Giant Monster Bug)</option>
+        <option value="High">High</option>
+        <option value="Medium">Medium</option>
+        <option value="Low">Low</option>
+        </select><br><br>
+        <input type="submit" value="Report" class="bugkiller-button">
+        </form>
+</body>
+</html>
