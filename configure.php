c758a00f4defabb843bf7b058eba76d5083a176d
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-06-24 21:42:56 +0100

    Update configure.php

--- configure.php
+++ configure.php
@@ -2,6 +2,11 @@
 // Script to auto-configure your bugkiller pages.
 // Do not modify this file - if you want to edit your bugkiller configuration,
 // edit config.ini which contains your config values.
+function isSecure() {
+  return
+    (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+    || $_SERVER['SERVER_PORT'] == 443;
+}
 $config = parse_ini_file("config.ini");
 $servername = $config['servername'];
 $username = $config['username'];
@@ -9,6 +14,11 @@ $password = $config['password'];
 $dbname = $config['dbname'];
 $projectname = $config['projectname'];
 $path = "//" . $config['path'];
+if (isSecure()) {
+  $pathwithhttp = "https://" . $config['path'];
+} else {
+  $pathwithhttp = "http://" . $config['path'];
+}
 if ($password == "") {
   header("HTTP/1.1 503 Service Unavailable");
   echo "<strong>Note for users: $projectname is attempting to resolve a security issue related to this Bugkiller. Please wait until the site comes back online.</strong><br><br><strong>Note for the operator: For security reasons, it is no longer possible to use Bugkiller without a MySQL database password.</strong><br>Please alter the <code>$username</code> user to have a password.";
