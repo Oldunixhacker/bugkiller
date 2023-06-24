e89596c6c1b565582c5e89e4436ed8740b0de1ac
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-06-24 21:50:08 +0100

    Update view.php

--- view.php
+++ view.php
@@ -4,7 +4,7 @@ require_once "configure.php";
 $url = $_SERVER['REQUEST_URI'];
 $path = parse_url($url, PHP_URL_PATH);
 $segments = explode('/', $path);
-$arg = $segments[2];
+$arg = utf8_decode(urldecode($segments[2]));
 if ($arg == "") {
   header("Location: $pathwithhttp/search.php");
   exit;
