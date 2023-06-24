9530f6fb913070b230960fdcb9becc9c6ab62b8b
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-06-25 00:14:12 +0100

    Update search.php

--- search.php
+++ search.php
@@ -52,7 +52,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "<p>$resultcount results found.</p>";
       }
       while ($row = mysqli_fetch_assoc($result)) {
-        echo "<a style=\"font-size: 2em;\" href=\"" . $path . "/view.php/" . $row["id"] . "\">" . $row["bug_name"] . "</a><br><span class=\"search-result-description\">" . substr($row["bug_description"], 0, 20) . "</span><br><br>";
+        echo "<a style=\"font-size: 2em;\" href=\"" . $path . "/view.php/" . $row["id"] . "\">" . $row["bug_name"] . "</a><br><span class=\"search-result-description\">" . substr($row["bug_description"], 0, 55) . "</span><br><br>";
       }
       echo "<p>You can <a href=\"$path/reportbug.php\">report a bug</a> if these search results did not help.</p>";
     } else {
