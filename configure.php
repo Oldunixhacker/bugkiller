54cf5fb2c8dbf7ae31345b3ff56b7545765ec22c
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-06-24 16:16:33 +0100

    Update configure.php

--- configure.php
+++ configure.php
@@ -14,5 +14,5 @@ if ($password == "") {
   echo "<strong>Note for users: $projectname is attempting to resolve a security issue related to this Bugkiller. Please wait until the site comes back online.</strong><br><br><strong>Note for the operator: For security reasons, it is no longer possible to use Bugkiller without a MySQL database password.</strong><br>Please alter the <code>$username</code> user to have a password.";
   exit;
 };
-echo "<span style="visibility: none;">Path defined in config.ini: $path</span>";
+echo "<span style=\"visibility: none;\">Path defined in config.ini: $path</span>";
 ?>
