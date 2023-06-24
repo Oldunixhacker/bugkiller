e99b7d446249ba052e9f35a479c1c60a78b26e5a
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-06-24 15:19:14 +0100

    Create configure.php

---
+++ configure.php
@@ -0,0 +1,12 @@
+<?php
+// Script to auto-configure your bugkiller pages.
+// Do not modify this file - if you want to edit your bugkiller configuration,
+// edit config.ini which contains your config values.
+$config = parse_ini_file("config.ini");
+$servername = $config['servername'];
+$username = $config['username'];
+$password = $config['password'];
+$dbname = $config['dbname'];
+$projectname = $config['projectname'];
+$path = $config['path'];
+?>
