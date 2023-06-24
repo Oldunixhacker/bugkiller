ced02e62e38dd0c3154af079968c40f679e7da8c
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-06-24 15:47:24 +0100

    Update configure.php

--- configure.php
+++ configure.php
@@ -8,9 +8,10 @@ $username = $config['username'];
 $password = $config['password'];
 $dbname = $config['dbname'];
 $projectname = $config['projectname'];
-$path = $config['path'];
+$path = "//" . $config['path'];
 if ($password == "") {
-  echo "<strong>For security reasons, it is no longer possible to use Bugkiller without a MySQL database password.</strong><br>Please alter the <code>$username</code> user to have a password.";
-  exit 1;
+  header("HTTP/1.1 503 Service Unavailable");
+  echo "<strong>Note for users: $projectname is attempting to resolve a security issue related to this Bugkiller. Please wait until the site comes back online.</strong><br><br><strong>Note for the operator: For security reasons, it is no longer possible to use Bugkiller without a MySQL database password.</strong><br>Please alter the <code>$username</code> user to have a password.";
+  exit;
 }
 ?>
