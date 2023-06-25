01555f2b89d67311e686a49c90c214335e4d1de8
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-06-25 10:27:48 +0100

    Update view.php

--- view.php
+++ view.php
@@ -11,9 +11,25 @@ if ($arg == "") {
   exit;
 }
 if (!is_numeric($arg)) {
-  echo "<p><b>Bad bug identifier:</b> Bug IDs must be an Arabic numeral.</p>";
+  echo "<p><b>Bad bug identifier:</b> Due to the nature of how the Bugkiller database works, bug IDs must be an Arabic numeral (1, 2, 3, and so on).</p>";
   header("HTTP/1.1 400 Bad Request");
   exit;
 }
-echo "Got $arg";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  echo "Unable to view bug: " . $conn->connect_error;
+  exit;
+}
+
+$stmt = $pdo->prepare('SELECT bug_title, bug_description FROM bugs WHERE id = :id');
+$stmt->execute(['id' => $arg]);
+$bug = $stmt->fetch();
+if ($bug) {
+    echo '<h1>' . htmlspecialchars($bug['bug_title']) . '</h1>';
+    echo '<pre><code>' . htmlspecialchars($bug['bug_description']) . '</code></pre>';
+} else {
+    echo "There is no bug with the ID provided.";
+}
 ?>
