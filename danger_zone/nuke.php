7f3144b68aa217042926a0724710a597895e4656
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-06-26 07:35:44 +0000

    fix paths in nuke.php

--- danger_zone/nuke.php
+++ danger_zone/nuke.php
@@ -1,6 +1,6 @@
 <?php
-require_once "topbar.php";
-require_once "configure.php";
+require_once "../topbar.php";
+require_once "../configure.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $conn = new mysqli($servername, $username, $password, $dbname);
