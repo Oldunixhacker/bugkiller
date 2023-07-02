11fa63e5f5ff6b43f2b137c78161a72804ceae5a
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-07-02 09:21:10 +0100

    Fix typo

--- view.php
+++ view.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="/style.css">
 <?php
-require "vender/autoload.php";
+require "vendor/autoload.php";
 require_once "topbar.php";
 require_once "configure.php";
 $iswiki = $config['wikitextallowed'];
