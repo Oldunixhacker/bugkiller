057c5258287f9594f5b0f88aeb57f8a0bd042a75
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-07-02 13:32:12 +0100

    Create reportbug.js

---
+++ reportbug.js
@@ -0,0 +1,4 @@
+function captchaResize() {
+  var captchaCodeWidth = document.getElementById("captcha-code").offsetWidth;
+  document.getElementById("captcha").style.width = captchaCodeWidth + "px";
+}
