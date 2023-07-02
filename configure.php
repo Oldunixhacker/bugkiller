fedd4a6298aae363a5518eda92cfe119904bf722
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-07-02 12:50:19 +0100

    Update configure.php

--- configure.php
+++ configure.php
@@ -25,9 +25,7 @@ if (isSecure()) {
 } else {
   $pathwithhttp = "http://" . $config['path'];
 }
-if (isMobile()) {
-    header("Location: $pathwithhttp/m");
-}
+$mobile = isMobile();
 if ($password == "") {
   header("HTTP/1.1 503 Service Unavailable");
   echo "<strong>Note for users: $projectname is attempting to resolve a security issue related to this Bugkiller. Please wait until the site comes back online.</strong><br><br><strong>Note for the operator: For security reasons, it is no longer possible to use Bugkiller without a MySQL database password.</strong><br>Please alter the <code>$username</code> user to have a password.";
