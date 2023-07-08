374f54b24156dc2712fdd703a459f4cc7f854035
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-07-08 11:57:00 +0100

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php require "configure.php"; require "topbar.php"; ?>
+
+<h1>
+404 Not Found
+</h1>
+
+<p>Remember, a report a day keeps the bugs away.<br><a href="<?php echo $path; ?>">Stay home.</a></p>
