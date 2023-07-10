ea99e895437e544c5851cfaa8308b3948825ff1a
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-07-10 19:11:02 +0100

    Update update.php

--- maintenance/update.php
+++ maintenance/update.php
@@ -5,13 +5,21 @@ if (PHP_SAPI == 'apache2handler') {
    echo "BUGKILLER MAINTENANCE\n\nPlease run maintenance scripts from your server's command-line shell.\nContact your administrator if you do not have access.";
    exit;
 }
-if (PHP_SAPI != 'cli') {
+if (PHP_SAPI != 'cli' and PHP_SAPI != 'cli-server') {
    $runtype = PHP_SAPI;
    echo "Run this script via the command-line shell. If you do not have access to your server's shell contact your server administrator. Detected PHP_SAPI: $runtype\n";
    exit;
 }
+if (posix_getuid() !== 0 and PHP_SAPI == 'cli-server') {
+   echo "Please run the PHP server as root.\n";
+   exit;
+}
 if (posix_getuid() !== 0) {
    echo "Please run this script as root.\n";
+   exit;
+}
+if (PHP_SAPI == 'cli-server') {
+   require "../updateFromCliServer.php";
    exit;
 }
 echo "Bugkiller Server Updater\n";
