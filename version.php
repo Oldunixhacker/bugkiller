5c42163fa264f28e7cf1183576c6ce788616b78c
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-07-11 14:44:33 +0100

    Update version.php

--- version.php
+++ version.php
@@ -4,7 +4,7 @@ require "./vendor/autoload.php";
 require_once "topbar.php";
 require_once "configure.php";
 ?>
-<link rel="stylesheet" href='<?php echo "$path"; ?>'>
+<link rel="stylesheet" href='style.css'>
 <h1>License</h1>
 <pre><code>
 <?php
@@ -33,3 +33,43 @@ echo '</tbody>';
 echo '</table>';
 ?>
 <h1>Composer packages</h1>
+<?php
+
+// Get the list of installed Composer packages.
+$packages = shell_exec('composer show -i');
+
+// Split the list of packages into an array.
+$packages = explode("\n", $packages);
+
+// Create an HTML table.
+echo '<table border="1">';
+echo '<thead>';
+echo '<tr>';
+echo '<th>Package</th>';
+echo '<th>Description</th>';
+echo '<th>Version</th>';
+echo '<th>License</th>';
+echo '</tr>';
+echo '</thead>';
+echo '<tbody>';
+
+// Loop through the list of packages and add them to the table.
+foreach ($packages as $package) {
+  $parts = explode(' ', $package);
+  $name = $parts[0];
+  $version = $parts[1];
+  $description = $parts[2];
+  $license = $parts[3];
+
+  echo '<tr>';
+  echo '<td>' . $name . '</td>';
+  echo '<td>' . $description . '</td>';
+  echo '<td>' . $version . '</td>';
+  echo '<td>' . $license . '</td>';
+  echo '</tr>';
+}
+
+echo '</tbody>';
+echo '</table>';
+
+?>
