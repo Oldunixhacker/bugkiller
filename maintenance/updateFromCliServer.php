5a5701f4f0b0d0bfa8c5d00c6746b0fcf61ff6ed
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-07-11 13:44:14 +0100

    Update updateFromCliServer.php

--- maintenance/updateFromCliServer.php
+++ maintenance/updateFromCliServer.php
@@ -7,9 +7,7 @@ if (posix_getuid() !== 0) {
    echo "Please run the development server as root.\n";
    exit;
 }
-header("HTTP/1.1 200 OK");
 echo "<h1>Bugkiller Web Upgrade</h1>";
-echo "<p>You are upgrading Bugkiller from an external server. Please check your command-line shell for prompts.</p>";
 chdir(dirname(__DIR__));
 echo "<h3>Update packages</h3><pre><code>";
 echo nl2br(shell_exec("composer update --no-dev"));
@@ -20,6 +18,5 @@ echo nl2br(shell_exec("git reset"));
 echo "\n";
 echo nl2br(shell_exec("git pull"));
 echo "</code></pre>";
-echo "<h3>Restart Apache</h3>";
-shell_exec("apachectl restart");
+echo "<h3>Complete</h3>";
 echo "The update has been completed.";
