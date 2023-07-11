c4b25cbf43d4b1087f0d5dff5ee9af88ca487195
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-07-11 14:27:30 +0100

    Update search.php

--- search.php
+++ search.php
@@ -54,7 +54,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "<p>$resultcount results found.</p>";
       }
       while ($row = mysqli_fetch_assoc($result)) {
-        echo "<a style=\"font-size: 2em;\" href=\"" . $path . "/view.php/" . $row["id"] . "\">" . $row["bug_name"] . "</a> <span style=\"opacity: 70%; font-family: monospace\">#" . $row["id"] . "</span><br><span class=\"search-result-description\">" . htmlspecialchars(substr($row["bug_description"], 0, 55)) . "</span><br><br>";
+        echo "<a style=\"font-size: 2em;\" href=\"" . $path . "/" . $row["id"] . "\">" . $row["bug_name"] . "</a> <span style=\"opacity: 70%; font-family: monospace\">#" . $row["id"] . "</span><br><span class=\"search-result-description\">" . htmlspecialchars(substr($row["bug_description"], 0, 55)) . "</span><br><br>";
       }
       echo "<p>You can <a href=\"$path/reportbug.php\">report a bug</a> if these search results did not help.</p>";
     } else {
