3f72121ab61aa332bf07384e526178a5ec6164fc
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-07-16 02:00:38 +0100

    Update reportbug.js

--- reportbug.js
+++ reportbug.js
@@ -1,4 +1,5 @@
 function captchaResize() {
   var captchaCodeWidth = document.getElementById("bugkiller-captcha-code").offsetWidth;
   document.getElementById("captcha").style.width = captchaCodeWidth + "px";
+  document.getElementById("captcha").classList.add("bugkiller-captcha-input-modified");
 }
